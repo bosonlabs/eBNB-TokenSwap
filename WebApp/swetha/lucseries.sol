d54e1f593b55ff5bb536654cc218c2ca60fce4a8
swethakarthi <swethakarthi05@gmail.com>
2021-08-16 21:52:07 +0530

    lucseries

---
+++ WebApp/swetha/lucseries.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.22;
+contract Lucas {
+uint[] lucseries;
+
+  function generateLuc(uint n) public {  // n = how many in the series to return
+
+    // set 1st and 2nd entries
+    lucseries.push(0);  
+    lucseries.push(1);
+
+    // generate subsequent entries
+    for (uint i=2; i < n ; i++) {
+      lucseries.push(lucseries[i-1] + lucseries[i-2]);
+    }
+   }
+   function getluc() public view returns(uint[]){
+       return lucseries;
+   }
+} 
